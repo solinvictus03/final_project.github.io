b22e1571f398850f4e9d7da464d78406b2a95b7d
Shyngys001 <93879495+Shyngys001@users.noreply.github.com>
2022-12-19 20:19:31 +0600

    Update gameCard.js

--- gameCard.js
+++ gameCard.js
@@ -1,5 +1,6 @@
 export const createGameCard = (defaultIcon, images) => {
     const card = document.createElement('div');
     card.classList.add('game-card');
-
+    const notFlippedCardI = document.createElement('div');
+    const flippedImage = document.createElement('img');
 }
