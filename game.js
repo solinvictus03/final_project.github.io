69fe006ddd1ece2cb06fe17af309d49dbf4ec08e
Shyngys001 <93879495+Shyngys001@users.noreply.github.com>
2022-12-19 14:46:30 +0600

    Update game.js

--- game.js
+++ game.js
@@ -2,4 +2,13 @@
 import { createGameCard } from "./gameCard.js";
 import { createImagesArray, duplicateArray, shuffle } from "./utils.js";
 
- 
+ const startGame = () => {
+    let firstCard = null;
+    let secondCard = null;
+    let clickable = true;
+
+    const gameSection = document.querySelector('.game-section__container');
+    const gameTable = document.createElement('div');
+    const cardsIcons = createImagesArray();
+    const duplicatedCardsIcons = duplicateArray(cardsIcons);
+    const restartBtn = document.createElement('button');
